b9eeff3887cc703225649a7b20c28dc31f7a4429
Micheline <MDoughlin@hotmail.com>
2025-09-29 23:40:55 -0400

    Adding Styling to Menu page

--- app/(tabs)/menu.tsx
+++ app/(tabs)/menu.tsx
@@ -10,13 +10,35 @@ import {
   View,
   StyleSheet,
   FlatList,
+  Image,
 } from "react-native";
 
 const VendorMenu = () => {
   const router = useRouter();
   // const { title } = useLocalSearchParams();
   const vendorId = useSelector((state: any) => state.vendor.vendorId);
-  const [menuItems, setMenuItems] = useState([]);
+  const [menuItems, setMenuItems] = useState([
+    {
+      id: "1",
+      item: "Cheeseburger",
+      price: "8.99",
+      imageUrl:
+        "https://californiaavocado.com/wp-content/uploads/2021/03/Classic-Calif-Avocado-Cheeseburger-with-Carmelized-Onions-CAC-001-cropped-1014x676.jpg",
+    },
+    {
+      id: "2",
+      item: "Veggie Wrap",
+      price: "6.49",
+      imageUrl:
+        "https://www.superhealthykids.com/wp-content/uploads/2023/08/veggie-wrap-3.jpg",
+    },
+    {
+      id: "3",
+      item: "Chicken Tenders",
+      price: "9.25",
+      imageUrl: "https://example.com/chicken-tenders.jpg",
+    },
+  ]);
 
   console.log("Redux vendorId", vendorId);
 
@@ -42,17 +64,23 @@ const VendorMenu = () => {
   return (
     <SafeAreaView>
       <View>{/* <Text>{title}</Text> */}</View>
-
       <FlatList
         data={menuItems}
         keyExtractor={(item) => item.id}
         contentContainerStyle={{ padding: 16 }}
         renderItem={({ item }) => (
           <View style={styles.menuItem}>
-            <Text style={styles.name}>{item.item}</Text>
-            <Text style={styles.price}>
-              ${parseFloat(item.price).toFixed(2)}
-            </Text>
+            <Image
+              source={{ uri: item.imageUrl }}
+              style={styles.image}
+              resizeMode="cover"
+            />
+            <View style={styles.textContainer}>
+              <Text style={styles.name}>{item.item}</Text>
+              <Text style={styles.price}>
+                ${parseFloat(item.price).toFixed(2)}
+              </Text>
+            </View>
           </View>
         )}
         ListEmptyComponent={
@@ -76,6 +104,29 @@ const VendorMenu = () => {
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  menuItem: {
+    flexDirection: "row",
+    alignItems: "flex-start",
+    marginBottom: 20,
+  },
+  image: {
+    width: 60,
+    height: 60,
+    borderRadius: 8,
+    marginRight: 12,
+  },
+  textContainer: {
+    flex: 1,
+  },
+  price: {
+    marginTop: 2,
+  },
+  name: {
+    fontWeight: "600",
+    fontSize: 16,
+    paddingTop: 3,
+  },
+});
 
 export default VendorMenu;
